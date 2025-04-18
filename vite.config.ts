fc1bf053827c685154fcc5fa1e0ec6df0823324a
haki9x <75064258+haki9x@users.noreply.github.com>
2025-04-18 15:36:55 +0700

    Sửa folder build
    Co-Authored-By: haki9x <75064258+haki9x@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // Specify the desired output directory
+    emptyOutDir: true, // Optional: Clears the output directory before building
+  },
+});
